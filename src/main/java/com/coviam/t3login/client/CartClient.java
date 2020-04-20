006d0620d52a392633a45970bf3da00101622f99
dhruvtailor7 <32268377+dhruvtailor7@users.noreply.github.com>
2020-04-20 18:43:36 +0530

    Update CartClient.java

--- src/main/java/com/coviam/t3login/client/CartClient.java
+++ src/main/java/com/coviam/t3login/client/CartClient.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("cart")
+@FeignClient("http://bookstore-cart.herokuapp.com/")
 public interface CartClient {
     @PostMapping({"/updateGuestCart"})
     void updateGuestCart(@RequestBody CartUpdateDTO var1);
